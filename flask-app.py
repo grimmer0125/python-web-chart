c835e697d87ac6891fbb6652d97e51936ce6d3d9
Grimmer <grimmer0125@gmail.com>
2018-07-03 23:37:41 +0800

    Add dropdown actions in flask version and add requirements.txt

--- flask-app.py
+++ flask-app.py
@@ -15,11 +15,25 @@ from flask import render_template
 def hello(name=None):
     return render_template('flask-hello.html', name=name)
 
-@app.route('/chart/')
-def chart():
+@app.route('/chart/mode1')
+def chart1():
     x = [-2,0,4,6,7]
     y = [q**2-q+3 for q in x]
     trace1 = go.Scatter(x=x, y=y, marker={'color': 'red', 'symbol': 104, 'size': "10"},
+                        mode="lines",  name='1st Trace')
+
+    data=go.Data([trace1])
+    layout=go.Layout(title="Meine Daten", xaxis={'title':'x1'}, yaxis={'title':'x2'})
+    figure=go.Figure(data=data,layout=layout)
+    graph = opy.plot(figure, auto_open=False, output_type='div')
+
+    return render_template('flask-chart.html', graph=graph)
+
+@app.route('/chart/mode2')
+def chart2():
+    x = [-2,0,4,6,7]
+    y = [q**2-q+3 for q in x]
+    trace1 = go.Scatter(x=x, y=y, marker={'color': 'green', 'symbol': 104, 'size': "10"},
                         mode="lines",  name='1st Trace')
 
     data=go.Data([trace1])
