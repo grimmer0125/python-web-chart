5c446538f5777665c38a18d866c446e5c308d7d8
Grimmer <grimmer0125@gmail.com>
2020-02-08 23:00:22 +0800

    Add material-ui button and hooks example code

--- reactui/src/App.jsx
+++ reactui/src/App.jsx
@@ -1,29 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
+import Button from "@material-ui/core/Button";
+
 import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
+  const [count, setCount] = useState(0);
+
+  function handleClick() {
+    setCount(count + 1);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <small>
-          {/* development or production */}
-          You are running this application in <b>{process.env.NODE_ENV}</b>{" "}
-          mode.
-        </small>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code> src / App.js </code> and save to reload.{" "}
-        </p>{" "}
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React{" "}
-        </a>{" "}
-      </header>{" "}
+      <p>You clicked {count} times</p>
+      <Button onClick={handleClick} variant="contained" color="primary">
+        Click me
+      </Button>
+      You are running this application in <b>{process.env.NODE_ENV}</b> mode.
     </div>
   );
 }
